5c88bb7bb391f683d5a6cad676604b9f805dfcd6
BavindyaWanninayaka <tharikawmbt@gmail.com>
2026-04-24 14:10:53 +0530

    login

---
+++ tests/My_First_Project.spec.js
@@ -0,0 +1,18 @@
+const {test, expect} = require('@playwright/test')
+
+test('My Test Case', async ({page}) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await expect(page).toHaveTitle('OrangeHRM');
+
+    await page.getByRole('textbox', { name: 'username' }).fill('admin');
+    await page.getByRole('textbox', { name: 'password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+
+
+
+ 
+
+} )
+
